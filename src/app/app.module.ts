3db498b182cd49bbb8dbe4eb46e6b17bde93c99a
ylin <ylin@HOOD>
2018-08-23 15:18:39 +0200

    add profile and subscription modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SubscriptionModule } from './component/subscription/subscription.module';
+import { ProfileModule } from './component/profile/profile.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SubscriptionModule,
+    ProfileModule
   ],
   providers: [],
   bootstrap: [AppComponent]
